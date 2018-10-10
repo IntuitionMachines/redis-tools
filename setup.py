95df436eeb0a527baa53b8caa1d091e754238711
Alex Newman <posix4e@gmail.com>
2018-10-10 16:25:46 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='redishelpers',
+      version='0.1',
+      description='A set of tools we use to easily access redis',
+      url='git@github.com:HCaptcha/redis-tools.git',
+      author='Intuition Machines',
+      author_email='contact@intuitionmachines.com',
+      license='MIT',
+      packages=['redishelpers'],
+      install_requires=["redis"],
+      zip_safe=False)
+
