aea38f76dc716532ef3d50494ade04ba3e4f35da
Alex Newman <posix4e@gmail.com>
2019-01-04 17:18:14 -0800

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hmtredishelp',
-      version='0.1.13',
+      version='0.1.16',
       description='A set of tools we use to easily access redis',
       url='https://github.com/IntuitionMachines/redis-tools',
       author='Intuition Machines',
