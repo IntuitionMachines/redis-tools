84528f45e143a7ad6ef3dd5f9eaa13fe2a6acec1
ali neer <ali@alineer.com>
2019-01-17 10:41:05 -0500

    lint

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='redis-tools',
-      version='1.0.0',
-      description='A set of tools we use to easily access redis',
-      url='https://github.com/IntuitionMachines/redis-tools',
-      author='Intuition Machines',
-      author_email='dev@intuitionmachines.com',
-      license='MIT',
-      packages=['redis-tools'],
-      install_requires=["redis"],
-      zip_safe=False)
-
+setup(
+    name='redis-tools',
+    version='1.0.0',
+    description='A set of tools we use to easily access redis',
+    url='https://github.com/IntuitionMachines/redis-tools',
+    author='Intuition Machines',
+    author_email='dev@intuitionmachines.com',
+    license='MIT',
+    packages=['redistools'],
+    install_requires=["redis"],
+    zip_safe=False)
